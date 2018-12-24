68755d19f9492c2263bc4326ac10753d77cf75d3
tarurar <tarutin_andrew@mail.ru>
2018-12-24 11:57:33 +0300

    increased CollectionUpdateMethod unit test coverage

--- src/tests/Ironclad.Tests/Unit/CollectionUpdateMethodTests.cs
+++ src/tests/Ironclad.Tests/Unit/CollectionUpdateMethodTests.cs
@@ -25,8 +25,56 @@ namespace Ironclad.Tests.Unit
             Assert.True(changeCollection.SequenceEqual(resultCollection));
         }
 
+        [Theory]
+        [InlineData(10, 1)]
+        [InlineData(10, 0)]
+        [InlineData(10, 10)]
+        public void ApplyTo_AddValues_ReturnsConcatenatedCollection(
+            int collectionLength, 
+            int crossValuesCount)
+        {
+            var sourceCollection = Enumerable.Range(1, collectionLength).Select(x => x.ToString()).ToList();
+
+            var changeCollection = Enumerable.Range(collectionLength + 1 - crossValuesCount, collectionLength)
+                .Select(x => x.ToString())
+                .ToList();
+
+            var updateMethod = CollectionUpdateMethod<string>.FromOptions(true, false, changeCollection);
+
+            var resultCollection = updateMethod.ApplyTo(sourceCollection);
+
+            Assert.Equal(2 * collectionLength - crossValuesCount, resultCollection.Count);
+
+            changeCollection.ForEach(x => Assert.True(resultCollection.Contains(x)));
+
+            sourceCollection.ForEach(x => Assert.True(resultCollection.Contains(x)));
+        }
+
+        [Theory]
+        [InlineData(10, 1)]
+        [InlineData(10, 0)]
+        [InlineData(10, 10)]
+        public void ApplyTo_RemoveValues_ReturnsOriginalCollectionButValuesRemoved(
+            int collectionLength,
+            int crossValuesCount)
+        {
+            var sourceCollection = Enumerable.Range(1, collectionLength).Select(x => x.ToString()).ToList();
+
+            var changeCollection = Enumerable.Range(collectionLength + 1 - crossValuesCount, collectionLength)
+                .Select(x => x.ToString())
+                .ToList();
+
+            var updateMethod = CollectionUpdateMethod<string>.FromOptions(false, true, changeCollection);
+
+            var resultCollection = updateMethod.ApplyTo(sourceCollection);
+
+            Assert.Equal(collectionLength - crossValuesCount, resultCollection.Count);
+
+            changeCollection.ForEach(x => Assert.False(resultCollection.Contains(x)));
+        }
+
         [Fact]
-        public void ApplyTo_AddNotCrossedValues_ReturnsConcatenatedCollection()
+        public void ApplyTo_InvalidOptions_ReturnsUnchangedCollection()
         {
             const int collectionLength = 10;
 
@@ -36,39 +84,11 @@ namespace Ironclad.Tests.Unit
                 .Select(x => x.ToString())
                 .ToList();
 
-            var updateMethod = CollectionUpdateMethod<string>.FromOptions(true, false, changeCollection);
+            var updateMethod = CollectionUpdateMethod<string>.FromOptions(true, true, changeCollection);
 
             var resultCollection = updateMethod.ApplyTo(sourceCollection);
 
-            Assert.Equal(sourceCollection.Count + changeCollection.Count, resultCollection.Count);
-
-            changeCollection.ForEach(x => Assert.True(resultCollection.Contains(x)));
-
-            sourceCollection.ForEach(x => Assert.True(resultCollection.Contains(x)));
-        }
-
-        [Fact]
-        public void ApplyTo_AddCrossedValues_ReturnsConcatenatedCollection()
-        {
-            // todo
-        }
-
-        [Fact]
-        public void ApplyTo_RemoveCrossedValues_ReturnsOriginalCollectionButValuesRemoved()
-        {
-            // todo
-        }
-
-        [Fact]
-        public void ApplyTo_RemoveNotCrossedValues_ReturnsOriginalCollectionButValuesRemoved()
-        {
-            // todo
-        }
-
-        [Fact]
-        public void ApplyTo_InvalidOptions_ReturnsUnchangedCollection()
-        {
-            // todo
+            Assert.True(sourceCollection.SequenceEqual(resultCollection));
         }
     }
 }
